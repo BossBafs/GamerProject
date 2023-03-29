56a211d10658a069d944e69f7e7fc851331f7f0b
M.Ali <matuncdemir@gmail.com>
2023-03-29 16:37:36 +0300

    Add project files.

---
+++ GamerProject/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace GamerProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            GamerManager gamerManager = new GamerManager(new UserValidationManager());
+            gamerManager.Add(new Gamer
+            {
+                BirthYear = 1996,
+                FirstName = "Muhammed Ali",
+                Id = 1,
+                LastName = "TUNÇDEMİR",
+                IdentityNumber = 1234567891
+            });
+
+            // Fake Doğrulama Oluşturup Sorgusunu Yaparsak;
+            GamerManager gamerManager1 = new GamerManager(new NewEStateUserValidationManager());
+            gamerManager1.Add(new Gamer 
+            { 
+                BirthYear = 2035, 
+                FirstName = "Muhammed Ali", 
+                Id = 1, LastName = "TUNÇDEMİR", 
+                IdentityNumber = 678910 
+            });
+        }
+    }
+}
---
+++ GamerProject/UserValidationManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GamerProject
+{
+    internal class UserValidationManager : IUserValidationService
+    {
+        public bool Validate(Gamer gamer)
+        {
+            if (gamer.BirthYear == 1996 && gamer.IdentityNumber==12345678910 && gamer.FirstName == "Muhammed Ali" && gamer.LastName == "TUNÇDEMİR")
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
